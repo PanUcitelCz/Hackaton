ff8f41c8b1d53b5cdbcbd587f3a846f1497c27e6
PanUcitelCz <104927244+PanUcitelCz@users.noreply.github.com>
2023-09-22 17:01:10 +0200

    Základ šablony

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '../lib/css/normallize.css';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,200;10..48,600&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<style lang="stylus">
+    
+</style>
