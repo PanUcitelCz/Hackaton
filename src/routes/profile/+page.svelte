be5ab316593b037a93b232fec1e23d54529dc300
PanUcitelCz <104927244+PanUcitelCz@users.noreply.github.com>
2023-09-23 04:45:14 +0200

    Profile beta

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
     export let profile: string;
-    import Cookies from 'js-cookie'
-
-    profile = String(Cookies.get("user"))
 
 </script>
 
@@ -21,12 +18,47 @@
         </div>
         <div class="hlasovaci_container">
             <div class="restaurace">
-                <div class="icon">
-                    <img src="https://lh5.googleusercontent.com/p/AF1QipPjMAY9OvOWdck9_IIk_w7HC27hcbcFAHxFYYdl=w122-h92-k-no" alt="">
-                </div>
-                <div class="nazev">
-                    <span>Restaurace</span>
-                </div>
+                    <div class="Header-card">
+                        <div class="icon">
+                            <img src="https://lh5.googleusercontent.com/p/AF1QipPjMAY9OvOWdck9_IIk_w7HC27hcbcFAHxFYYdl=w122-h92-k-no" alt="">
+                        </div>
+                        <div class="nazev">
+                            <div class="title">
+                                <h3>Restaurace</h3>
+                            </div>
+                            <div class="kadiboudy">
+                                <span>Průměr: </span>
+                                <span><img src="images/kadibouda.png" alt=""></span>
+                                <span><img src="images/kadibouda.png" alt=""></span>
+                                <span><img src="images/kadibouda.png" alt=""></span>
+                                <span><img src="images/kadibouda.png" alt=""></span>
+    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="Footer-card">
+                        <div class="odkazy">
+                            <span>
+                                <img src="images/web.svg" alt="">
+                            </span>
+                            
+                            <span>
+                                <img src="images/map.svg" alt="">
+                            </span>
+
+                            <span>
+                                <img src="images/coments.svg" alt="">
+                            </span>
+                        </div>
+                        <div class="hodnoceni">
+                            
+                            <div class="like">
+                                <span>35x</span>
+                                <span><img src="images/like.svg" alt=""></span>
+                            </div>
+                        </div>
+                    </div>
+                
             </div>
             <figure class="restaurace">
                 <div class="icon">
@@ -62,7 +94,7 @@
 
     .seznam
         width 100%
-        box-shadow 0px 2px 5px black
+        box-shadow 0px 2px 5px #8a0c0c
         border-radius 11px
 
     .container
@@ -90,6 +122,7 @@
         background-color #8a0c0c
         border-radius 10px 10px 0 0
         
+        
 
         h3
             padding 5px
@@ -97,11 +130,12 @@
             color white
 
     .restaurace
-        display flex
+        display grid
+        grid-template-columns: repeat(1, 1fr)
         width calc(100% - 50px)
         border-radius 10px
         min-height 150px
-        box-shadow 0px 2px 5px black
+        box-shadow 0px 2px 10px grey
         padding 10px
         
             
@@ -112,12 +146,82 @@
         align-items flex-start
 
         img
-            width 75%
             width 100px
             border-radius 10px
             max-height 100px
 
+            @media $large-up
+                width 250px
+
     .nazev
         padding 0 15px
+        width 100%
+        display flex
+        flex-direction column
+        align-items start
 
+        .title
+            display flex
+            align-items start
+
+        .nadpis
+            display flex
+            justify-content flex-start
+            align-items start
+
+
+
+        @media $large-up
+                padding 0 40px
+
+
+    .Header-card
+        display grid
+        grid-template-columns: 1fr 8fr
+
+    .Footer-card
+        display grid
+        grid-template-columns repeat(2, 1fr)
+        
+        .odkazy
+            display flex
+            align-items center
+            gap 6px
+
+            @media $large-up
+                gap 15px
+
+            span
+                color black
+
+                img
+                    width 30px
+                    display flex
+                    flex-direction row
+                    align-items center
+                    cursor pointer
+
+    .hodnoceni
+        display flex
+        justify-content flex-end
+        gap 24px
+
+        img
+            width 30px
+            cursor pointer
+
+    .kadiboudy
+        display flex
+        justify-content center
+        align-items center
+
+        img
+            width 30px
+            cursor pointer
+
+    .like
+        display flex
+        align-items center
+
+            
 </style>
